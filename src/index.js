fc11167fbd0c65cdbdba8cc13ba362a95ac3f74e
Александр <psychosocial555666@yandex.ru>
2020-06-10 10:08:53 +0700

    1.3.1 Отрисовывает главную страницу

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-const bodyElement = document.querySelector(`body`);
-bodyElement.textContent = `Hello, hello, hello!`;
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
+
+const init = () => {
+  const data = {
+    offersCount: 31,
+  };
+
+  ReactDOM.render(
+      <App offersCount = {data.offersCount}/>,
+      document.querySelector(`#root`)
+  );
+};
+
+init();
