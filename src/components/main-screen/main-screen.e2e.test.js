d9a2d360d159690cc53c38f8bb0e8e3454a7420c
Александр <psychosocial555666@yandex.ru>
2020-06-16 19:49:59 +0700

    2.2.1 Добавляет тесты для компонент

--- src/components/main-screen/main-screen.e2e.test.js
+++ src/components/main-screen/main-screen.e2e.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import MainScreen from "./main-screen";
 
@@ -18,18 +18,18 @@ describe(`MainScreenComponent`, () => {
   it(`Should header button be pressed`, () => {
     const onHeaderClick = jest.fn();
 
-    const welcomeScreen = shallow(
+    const welcomeScreen = mount(
         <MainScreen
           offersCount = {31}
           offersHeaders = {offersHeaders}
           onHeaderClick = {onHeaderClick}/>
     );
 
-    const headerButton = welcomeScreen.find(`tabs__item`);
+    const headerButtons = welcomeScreen.find(`.tabs__item`);
 
-    headerButton.forEach((it) => it.simulate(`click`));
+    headerButtons.forEach((it) => it.simulate(`click`, {preventDefault() {}}));
 
-    expect(onHeaderClick).toHaveBeenCalledTimes(1);
+    expect(onHeaderClick).toHaveBeenCalledTimes(headerButtons.length);
   });
 });
 
